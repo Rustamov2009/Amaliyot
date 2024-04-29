3a003aeaf853ddb3ab738197046b72b69250ba04
Rustamov2009 <firdavsr000@gmail.com>
2024-04-29 14:45:20 +0500

    amaliyot

---
+++ script.js
@@ -0,0 +1,46 @@
+// 1)   
+        // let a = 5;
+        // let b = 3;
+        // console.log(a + b);
+
+// 2)
+        // let a  =Number ('12')
+
+        // console.log(a)
+
+// 3)
+        // let son = Math.floor(Math.random() * 100); 
+        // if (son % 2 === 0) {
+        //     console.log(son + " juft son");
+        // } else {
+        //     console.log(son + " toq son");
+        // }
+
+// 4)
+        // let soz = prompt("Bir so'z kiriting:");
+        // if (soz === "Mars") {
+        //     console.log(soz);
+        // } else {
+        //     console.log("Mars IT School");
+        // }
+
+// 5)
+        // let mevalar = ["olma", "banan", "uzum", "olcha", "gilos"];
+        // let uzunMevalar = [];
+
+        // for (let meva of mevalar) {
+        //     if (meva.length >= 5) {
+        //         uzunMevalar.push(meva);
+        //     }
+        // }
+
+        // console.log(uzunMevalar);       
+
+// 6)
+        // for (let i = 1; i <= 100; i++) {
+        //     if (i % 2 === 0) {
+        //         console.log(i + " juft son");
+        //     } else {
+        //         console.log(i + " toq son");
+        //     }
+        // }
